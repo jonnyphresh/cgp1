9f5de537270c807a2c03680e0a5769621447b580
constantgrowthproductions <jon@constantgrowthproductions.com>
2018-07-25 16:11:45 -0700

    new build of the site

--- app/assets/scripts/App.js
+++
@@ -1,7 +0,0 @@
-var Person = require('./modules/Person')
-
-var jon = new Person("Jon Jennings", " blue");
-jon.greet();
-
-var heather= new Person("Heather Jennings", " green");
-heather.greet();
--- app/gulp/tasks/sprites.js
+++ app/gulp/tasks/sprites.js
@@ -1,23 +1,34 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
 
 var config = {
-    mode: {
-      css: {
-        render: {
-          css: {
-            template: './app/gulp/templates/sprite.css'
-          }
+  mode: {
+    css: {
+      sprite: 'sprite.svg',
+      render: {
+        css: {
+          template: './gulp/templates/sprite.css'
         }
       }
     }
+  }
 }
 
-gulp.task('createSprite', function () {
+gulp.task('beginClean', function() {
+  return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function() {
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
 gulp.task('copySpriteCSS', ['createSprite'], function() {
@@ -26,4 +37,8 @@ gulp.task('copySpriteCSS', ['createSprite'], function() {
     .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteCSS']);
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
+  return del('./app/temp/sprite');
+});
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
