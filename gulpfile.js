0c5d19bfc6bf4e092475a98a2ad0a7944d77752b
constantgrowthproductions <jon@constantgrowthproductions.com>
2018-07-25 17:08:11 -0700

    changed index.html;

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,3 @@
-require('./gulp/tasks/styles');
-require('./gulp/tasks/watch');
-require('./gulp/tasks/sprites');
-
-
-
-
-
-
-
-
-
-
+require('./app/gulp/tasks/styles');
+require('./app/gulp/tasks/watch');
+require('./app/gulp/tasks/sprites');
