1bd5f714327e471d8f2a1cca37d41f000c198ed9
constantgrowthproductions <jon@constantgrowthproductions.com>
2018-07-27 14:54:34 -0700

    added in some JS and made a lot happen with Gulp

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,12 @@
 var $ = require('jquery');
 var Person = require('./modules/Person');
 
+alert("ab321");
+
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
 
-$("h1").remove();
+/*$("h1").remove();*/
