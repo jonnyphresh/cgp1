ad3528ee3af24951077e3c13cdb79ec76d4ea56d
constantgrowthproductions <jon@constantgrowthproductions.com>
2018-07-29 09:00:24 -0700

    finished reveal-on-scroll functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -22,5 +22,9 @@ jane.payTaxes();
 $("h1").remove();
 */
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
