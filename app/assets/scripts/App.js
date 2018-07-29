4b7d3ea48556bf5e03af333da327756205f11325
constantgrowthproductions <jon@constantgrowthproductions.com>
2018-07-29 13:05:23 -0700

    finished modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,32 +1,11 @@
-/* This is all stuff from the course.
-
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-import Person from "./modules/Person";
-
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes .50 in taxes.");
-  }
-}
-
-alert("ab321");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
-*/
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,18 @@
-var path = require('path');
-
 module.exports = {
   entry: "./app/assets/scripts/App.js",
   output: {
-    path: path.resolve(__dirname, "./app/temp/scripts"),
+    path: "./app/temp/scripts",
     filename: "App.js"
   },
   module: {
     loaders: [
       {
-        loader: 'babel-loader',
-          query: {
-            presets: ['es2015']
-          },
-          test: /\.js$/,
-          exclude: /node_modules/
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
       }
     ]
   }
