c6c4562de5450d1a0f13cf903b06a96da336983f
jonnyphresh <jonathansdjennings@gmail.com>
2018-09-25 22:04:15 -0700

    arranged styling of top 2 sections

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 require('./gulp/tasks/styles');
+require('es6-promise').polyfill();
 require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
 require('./gulp/tasks/scripts');
 require('./gulp/tasks/modernizr');
 require('./gulp/tasks/build');
-
-
-
-
